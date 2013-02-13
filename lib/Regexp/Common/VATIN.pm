53796ef95670b96a1c37a62100ceb33aeead46f1
Richard Simões <rsimoes@hostgator.com>
2013-02-13 11:44:13 -0600

    frist

---
+++ lib/Regexp/Common/VATIN.pm
@@ -0,0 +1,35 @@
+package Regexp::Common::VATIN;
+
+use strict;
+use warnings FATAL => "all";
+use utf8;
+
+# VERSION
+# ABSTRACT:
+
+1;
+
+__END__
+
+=pod
+
+=encoding utf8
+
+=head1 NAME
+
+Regexp::Common::VATIN -
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 AUTHOR
+
+Richard Simões C<< <rsimoes AT cpan DOT org> >>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright © 2012 Richard Simões. This module is released under the terms of the
+L<GNU Lesser General Public License v. 3.0|http://gnu.org/licenses/lgpl.html>
+and may be modified and/or redistributed under the same or any compatible
+license.
